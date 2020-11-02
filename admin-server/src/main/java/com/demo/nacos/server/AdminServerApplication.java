af9fbc1294447713588cb1fb18d1e18f18a9b98b
Yoguruto <17673051831@163.com>
2020-11-02 19:58:47 +0800

    修改adminyaml

--- admin-server/src/main/java/com/demo/nacos/server/AdminServerApplication.java
+++ admin-server/src/main/java/com/demo/nacos/server/AdminServerApplication.java
@@ -2,13 +2,20 @@ package com.demo.nacos.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @Author ZXC
  * @Date 2020/10/31 16:06
  * @description
  */
-@EnableAutoConfiguration
+@ComponentScan({"com.demo.nacos"})
+@Configuration
+@SpringBootApplication
+@EnableDiscoveryClient
 public class AdminServerApplication {
 
     public static void main(String[] args) {
