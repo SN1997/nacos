e46fe03c4362810d0978f725ddfc697ad9c21981
q404219129 <404219129@qq.com>
2020-11-05 11:41:37 +0800

    nacos端口修改至pom文件

--- web-server/src/main/java/com/demo/nacos/server/WebServerApplication.java
+++ web-server/src/main/java/com/demo/nacos/server/WebServerApplication.java
@@ -1,7 +1,6 @@
 package com.demo.nacos.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -19,7 +18,7 @@ import org.springframework.context.annotation.Configuration;
 public class WebServerApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(WebServerApplication.class, args);
     }
+
 }
--- web-server/src/main/java/com/demo/nacos/server/web/WebController.java
+++ web-server/src/main/java/com/demo/nacos/server/web/WebController.java
@@ -1,15 +1,10 @@
 package com.demo.nacos.server.web;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,27 +17,14 @@ import java.util.Map;
 @RefreshScope
 public class WebController {
 
-    @Resource
-    private LoadBalancerClient loadBalancerClient;
-    @Resource
-    private RestTemplate restTemplate;
-
-    @Value("${common.name}")
+    @Value("${common.name:null}")
     private String config;
 
     @GetMapping("/config")
     public Map<String, Object> getConfig() {
         Map<String, Object> map = new HashMap();
-        map.put("ceshi", config);
+        map.put("config", config);
         return map;
-    }
-
-    @GetMapping("/hello/{message}")
-    public String hello(@PathVariable String message) {
-        ServiceInstance serviceInstance = loadBalancerClient.choose("web-server");
-        String path = String.format("http://%s:%s/web-server/%s", serviceInstance.getHost(), serviceInstance.getPort(), message);
-//        String result = restTemplate.getForObject(path, String.class);
-        return String.format(" %s from %s %s", message, serviceInstance.getHost(), serviceInstance.getPort());
     }
 
 }
